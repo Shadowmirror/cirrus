b306bdf61ea95e1bba6a3ed1dfd7d9fa90fffa5a
Your Name <miaoamirror@gmail.com>
2021-08-22 02:58:18 +0800

    喵

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
     maven { url = uri("https://kotlin.bintray.com/ktor") }
     maven { url = uri("https://kotlin.bintray.com/kotlinx") }
 }
